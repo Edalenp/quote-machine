d617f7e09838286354773eda5358df2f399877b0
Edalenp <eduardonegrin23@gmail.com>
2025-11-08 17:44:36 -0500

    Prettier changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import './App.css'
-import { FaQuoteLeft } from 'react-icons/fa'
+import "./App.css";
+import { FaQuoteLeft } from "react-icons/fa";
 import { FaSquareXTwitter } from "react-icons/fa6";
-import useData from './hooks/useData'
+import useData from "./hooks/useData";
 
 function App() {
-  const { quote, author, getData } = useData()
-  const handleClick = () => getData()
-  const tweetUrl = `https://twitter.com/intent/tweet?text=${encodeURIComponent(`"${quote}" - ${author}`)}`
+  const { quote, author, getData } = useData();
+  const handleClick = () => getData();
+  const tweetUrl = `https://twitter.com/intent/tweet?text=${encodeURIComponent(
+    `"${quote}" - ${author}`
+  )}`;
 
   return (
     <div className="main-box">
@@ -19,15 +21,22 @@ function App() {
           <p id="author">- {author}</p>
         </div>
         <div className="buttons">
-         <a className="button" id="tweet-quote" href={tweetUrl} target="_blank" rel="noopener noreferrer">
-          <FaSquareXTwitter id="twitter-icon"/> 
-         </a>
-        <button onClick={handleClick} id="new-quote">New Quote</button>
+          <a
+            className="button"
+            id="tweet-quote"
+            href={tweetUrl}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaSquareXTwitter id="twitter-icon" />
+          </a>
+          <button onClick={handleClick} id="new-quote">
+            New Quote
+          </button>
         </div>
       </div>
     </div>
-  )
-    
+  );
 }
 
-export default App
+export default App;
