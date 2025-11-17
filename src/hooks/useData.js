506cabcc963bbdd668c960e52dd3a6c0c7a7a967
Edalenp <eduardonegrin23@gmail.com>
2025-11-17 13:58:17 -0500

    Try/catch in fetching data

--- src/hooks/useData.js
+++ src/hooks/useData.js
@@ -2,26 +2,29 @@ import { useState, useEffect } from "react";
 
 const API_QUOTES = "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json";
 
-const getRandomQuote = (quotes) => quotes[Math.floor(Math.random() * quotes.length)]
+const getRandomQuote = (quotes) => quotes[Math.floor(Math.random() * quotes.length)];
 
 const useData = () => {
   const [quote, setQuote] = useState("");
   const [author, setAuthor] = useState("");
 
-  const getData = () => {
-    fetch(API_QUOTES)
-      .then(response => response.json())
-      .then(data => {
-        const quotes = data.quotes
-        const randomQuote = getRandomQuote(quotes)
-        setQuote(randomQuote.quote)
-        setAuthor(randomQuote.author)
-      })
-      .catch(error => console.log("Error fetching quote:", error))
+  const getData = async () => {
+    try {
+      const response = await fetch(API_QUOTES);
+      if (!response.ok) throw new Error(`Error status ${response.status}`);
+
+      const data = await response.json();
+      const randomQuote = getRandomQuote(data.quotes);
+      setQuote(randomQuote.quote);
+      setAuthor(randomQuote.author);
+
+    } catch (error) {
+      console.log("Error detected:", error)
+    }
   }
 
   useEffect(() => {
-    getData()
+    getData();
   }, [])
 
   return { quote, author, getData }
